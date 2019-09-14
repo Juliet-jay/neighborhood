5a6ca3b97032779778e9efa4f71b9c5703d467b6
kathure <juliekmuthee@gmail.com>
2019-09-15 00:22:51 +0300

    create single photo url

--- hood/urls.py
+++ hood/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$',views.welcome,name = 'welcome'),
     url(r'^home/(?P<neighbourhood_id>[0-9]+)$', views.home,name= 'home'),
+    url(r'^photo/(\d+',views.single_photo, name='single_photo'),
+    
 ]
