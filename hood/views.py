c2e413c982af467b89ca708b63179b81b30bbbf3
kathure <juliekmuthee@gmail.com>
2019-09-15 00:20:45 +0300

    define single photo function

--- hood/views.py
+++ hood/views.py
@@ -6,6 +6,12 @@ def welcome(request):
     return render(request, 'welcome.html')
 
 def home(request,Neighbourhood_id):
-    Posts = Post.objects.filter(Neighbourhood=Neighbourhood_).all()
+    posts = Post.objects.filter(Neighbourhood=Neighbourhood_).all()
     print(posts)
     return render(request,'index.html',{"posts":posts})
+
+def single_photo(request,post_id):
+    photo = Post.objects.get(id=post_id)
+    return render(request,'photo_details.html',
+    {'photo':photo})
+    
