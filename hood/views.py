d73d5f25e841da14cab1bc6acc92df67445bce8d
kathure <juliekmuthee@gmail.com>
2019-09-15 00:03:43 +0300

    define welcome function

--- hood/views.py
+++ hood/views.py
@@ -4,3 +4,8 @@ from .models import Neighbourhood
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def home(request,Neighbourhood_id):
+    Posts = Post.objects.filter(Neighbourhood=Neighbourhood_).all()
+    print(posts)
+    return render(request,'index.html',{"posts":posts})
