54e5c0ae218900a1ddb00fd4b3e67af675886719
kathure <juliekmuthee@gmail.com>
2019-09-15 00:05:51 +0300

    create url function for welcome

--- hood/urls.py
+++ hood/urls.py
@@ -1,2 +1,7 @@
 from django.conf.urls import url
 from . import views
+
+urlpatterns = [
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^home/(?P<neighbourhood_id>[0-9]+)$', views.home,name= 'home'),
+]
