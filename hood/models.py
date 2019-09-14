05c97912ec88aebf7e935a56e03aa61f2b0e55a6
kathure <juliekmuthee@gmail.com>
2019-09-15 02:12:25 +0300

    create class post

--- hood/models.py
+++ hood/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Neighbourhood(models.Models):
+class Neighbourhood(models.Model):
     neighbourhood_name = models.CharField(max_length = 50)
     neighbourhood_location = models.CharField(max_length = 50)
     occupants_count = models.IntegerField()
@@ -32,7 +32,28 @@ class Neighbourhood(models.Models):
         '''
         Neighbourhoods = cls.objects.all()
         return Neighbourhoods
-        
     
+    class Post(models.Model):
+        name = models.CharField(max_length =50)
+        post = models.ImageField(upload_to='posts/')
+        profile = models.ForeignKey('Profile')
+        post_caption = models.ForeignKey('Neighbourhood')
+        date = models.CharField(max_lenght=20)
+        business = models.ForeignKey('Business')
+        user = models.ForeignKey(User,on_delete=models.CASCADE)
+        
+        def__str__(self):
+            return self.name
+        
+        @classmethod
+        def get_post(cls):
+            images = Post.object.all()
+            return images
+        
+    @classmethod
+    def get_post_by_id(cls, id):
+    selected_post = Post.objects.filter_by(id=id)
+    return selected_post
+
     
     
