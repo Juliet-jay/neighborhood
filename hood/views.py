f57a39e1518d32a31b98ac8ef18c68a99fb3593a
kathure <juliekmuthee@gmail.com>
2019-09-14 21:24:21 +0300

    define welcome function

--- hood/views.py
+++ hood/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render,get_object_or_404
 from .models import Neighbourhood
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
