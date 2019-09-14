789686548c0c876d4fbd990a891a536ac433f923
kathure <juliekmuthee@gmail.com>
2019-09-14 20:38:01 +0300

    created folder structure

---
+++ hood/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Neighbourhood(models.Models):
+    neighbourhood_name = models.CharField(max_length = 50)
+    neighbourhood_location = models.CharField(max_length = 50)
+    occupants_count = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+      
+    def __str__(self):
+        return self.neighbourhood_name
+    
+    
+    
