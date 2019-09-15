4a6a2759c24842d63c808fba44c52e50a47a2257
kathure <juliekmuthee@gmail.com>
2019-09-15 12:10:50 +0300

    create a search results view

--- hood/views.py
+++ hood/views.py
@@ -21,10 +21,10 @@ def search_results(request):
         searched_businesses = Business.search_by_neighbourhood(search_term)
         message = f"{search_term}"
         
-        return render(request, 'search-results.html',{"message":message,"businesses":searched_businesses})
+        return render(request, 'search.html',{"message":message,"businesses":searched_businesses})
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search-results.html',{"message":message})
+        return render(request, 'search.html',{"message":message})
     
     
     
