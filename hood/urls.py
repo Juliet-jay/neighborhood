13f92ea5f44ed3fded556b8f890269319dca217b
kathure <juliekmuthee@gmail.com>
2019-09-15 11:59:30 +0300

    create search results url

--- hood/urls.py
+++ hood/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$',views.welcome,name = 'welcome'),
     url(r'^home/(?P<neighbourhood_id>[0-9]+)$', views.home,name= 'home'),
     url(r'^photo/(\d+)', views.single_photo, name='single_photo'),
+    url(r'^search_results/',views.search_results,name='search_results'),
     
 ]
