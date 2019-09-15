b01aee6d65af85945413a5f6092c86cb3a1369f6
kathure <juliekmuthee@gmail.com>
2019-09-15 16:02:42 +0300

    create neighbourhood url

--- hood/urls.py
+++ hood/urls.py
@@ -1,10 +1,21 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$',views.welcome,name = 'welcome'),
     url(r'^home/(?P<neighbourhood_id>[0-9]+)$', views.home,name= 'home'),
     url(r'^photo/(\d+)', views.single_photo, name='single_photo'),
     url(r'^search_results/',views.search_results,name='search_results'),
+    url(r'^profile/', views.profile, name='profile'),
+    url(r'^updateProfile/', views.create_profile, name="createProfile"),
+    url(r'^businesses/$', views.businesses, name= 'businesses'),
+    url(r'^neighbourhoods/',views.neighbourhoods, name="neighbourhoods"),
+
     
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
